6c359fa29ab610fa564c478600e484dcf16256ba
Mattias Carlsson <mattias.carlsson01@gmail.com>
2024-06-01 14:53:02 +0200

    refactor(ExerciseApplication): remove unused imports

--- src/main/java/com/paf/exercise/ExerciseApplication.java
+++ src/main/java/com/paf/exercise/ExerciseApplication.java
@@ -1,20 +1,8 @@
 package com.paf.exercise;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
 
 @SpringBootApplication
 @EnableCaching
